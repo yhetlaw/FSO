cee6532c65e8aba58af456a6e2f0a6e4b6d68beb
yhetlaw <jandresantos14@gmail.com>
2021-08-27 16:21:16 +0100

    2.12 unfinished

--- part2/data-for-countries/src/components/Search.js
+++ part2/data-for-countries/src/components/Search.js
@@ -3,18 +3,38 @@ import React from 'react';
 const Search = ({ countries }) => {
   const handleSearchChange = (event) => {
     const newSearch = event.target.value.toUpperCase();
-    let regex = new RegExp(newSearch);
+    const listCountries = document.getElementById('listCountries');
+    const tooMany = document.getElementById('tooMany');
+    const inputSearch = document.getElementById('inputSearch');
+    const regex = new RegExp(newSearch);
     const allCountries = countries
-      .map((country) => `<li>${country.name.toUpperCase()}</li>`)
-      .filter((country) => country.match(regex))
-      .join('');
-    document.getElementById('result').innerHTML = allCountries;
+      .map((country) => country.name.toUpperCase())
+      .filter((country) => country.match(regex));
+    console.log(allCountries);
+
+    /* allCountries.length > 0 && allCountries.length <= 10
+      ? (listCountries.innerHTML = allCountries.map((country) => `<li>${country}</li>`).join('')) &&
+        (tooMany.innerHTML = '')
+      : (tooMany.innerHTML = 'Too many catches, specify another filter') &&
+        (listCountries.innerHTML = ''); */
+
+    if (inputSearch === '') {
+      listCountries.innerHTML = '';
+      tooMany.innerHTML = '';
+    } else if (allCountries.length > 0 && allCountries.length <= 10) {
+      listCountries.innerHTML = allCountries.map((country) => `<li>${country}</li>`).join('');
+      tooMany.innerHTML = '';
+    } else {
+      tooMany.innerHTML = 'Too many catches, specify another filter';
+      listCountries.innerHTML = '';
+    }
   };
   return (
     <div>
       Search for countries:
-      <input onChange={handleSearchChange} />
-      <ul id='result'></ul>
+      <input id='inputSearch' onChange={handleSearchChange} />
+      <ul id='listCountries'></ul>
+      <p id='tooMany'></p>
     </div>
   );
 };
