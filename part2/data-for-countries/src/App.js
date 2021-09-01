922c5ad882773fa50627db7c9bb279144c9f6561
yhetlaw <jandresantos14@gmail.com>
2021-09-01 16:17:00 +0100

    2.12 unfinished

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,11 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Search from './components/Search';
 
-const App = (props) => {
+const Info = () => {
+  return (
+    <div>
+      <h1>Portugal</h1>
+      <h2>200000</h2>
+    </div>
+  );
+};
+
+const Search = ({ countries }) => {
+  const listCountries = document.getElementById('listCountries');
+  const result = document.getElementById('result');
+  const inputSearch = document.getElementById('inputSearch');
+  const info = document.getElementById('info');
+
+  const handleSearchChange = (event) => {
+    const newSearch = event.target.value.toLowerCase();
+    const allCountries = countries.filter((country) => country.name.toLowerCase().match(newSearch));
+
+    console.log(allCountries, allCountries.length);
+
+    if (inputSearch === '') {
+      listCountries.innerHTML = '';
+      result.innerHTML = '';
+    } else if (allCountries.length === 1) {
+      result.innerHTML = `<h1>${allCountries[0].name}</h1>
+      <p>The capital of ${allCountries[0].name} is ${allCountries[0].capital}</p>
+      <p>The population of ${allCountries[0].name} is ${allCountries[0].population} people</p>
+      <h2>Languages</h2>
+      <ul>
+        ${allCountries[0].languages.map((language) => `<li>${language.name}</li>`).join('')}
+      </ul>
+      <img src="${allCountries[0].flag}" alt="${allCountries[0].name}" width="170" height="150"> `;
+      console.log(allCountries[0]);
+      listCountries.innerHTML = '';
+      info.innerHTML = '<Info />';
+    } else if (allCountries.length > 0 && allCountries.length <= 10 && allCountries.length !== 1) {
+      listCountries.innerHTML = allCountries.map((country) => `<li>${country.name}</li>`).join('');
+      result.innerHTML = '';
+    } else {
+      result.innerHTML = 'Too many catches, specify another filter';
+      listCountries.innerHTML = '';
+    }
+  };
+
+  return (
+    <div>
+      Search for countries:
+      <input id='inputSearch' onChange={handleSearchChange} />
+      <ul id='listCountries'></ul>
+      <div id='result'></div>
+      <div id='info'></div>
+    </div>
+  );
+};
+
+const App = () => {
   const [countries, setCountries] = useState([]);
 
-  //Render the countries from the API
   useEffect(() => {
     console.log('effect');
     axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
--- part2/data-for-countries/src/components/Image.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Image = (props) => {
-  return <img src={props.country} alt='Girl in a jacket' width='200' height='200' />;
-};
-
-export default Image;
