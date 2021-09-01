e98babd2714a6286f48f82f6e4a7f4ad23115a63
yhetlaw <jandresantos14@gmail.com>
2021-09-01 16:38:40 +0100

    2.12 refactored

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,49 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const Search = ({ countries }) => {
-  const listCountries = document.getElementById('listCountries');
-  const result = document.getElementById('result');
-  const inputSearch = document.getElementById('inputSearch');
-
-  const handleSearchChange = (event) => {
-    const newSearch = event.target.value.toLowerCase();
-    const matchedCountries = countries.filter((country) => country.name.toLowerCase().match(newSearch));
-    if (inputSearch === '') {
-      listCountries.innerHTML = '';
-      result.innerHTML = '';
-    } else if (matchedCountries.length === 1) {
-      result.innerHTML = `<h1>${matchedCountries[0].name}</h1>
-      <p>The capital of ${matchedCountries[0].name} is ${matchedCountries[0].capital}</p>
-      <p>The population of ${matchedCountries[0].name} is ${matchedCountries[0].population} people</p>
-      <h2>Languages</h2>
-      <ul>
-        ${matchedCountries[0].languages.map((language) => `<li>${language.name}</li>`).join('')}
-      </ul>
-      <img src="${matchedCountries[0].flag}" alt="${matchedCountries[0].name}" width="170" height="150"> `;
-      listCountries.innerHTML = '';
-    } else if (matchedCountries.length > 0 && matchedCountries.length <= 10 && matchedCountries.length !== 1) {
-      listCountries.innerHTML = matchedCountries.map((country) => `<li>${country.name}</li>`).join('');
-      result.innerHTML = '';
-    } else {
-      result.innerHTML = 'Too many catches, specify another filter';
-      listCountries.innerHTML = '';
-    }
-  };
-
-  return (
-    <div>
-      Search for countries:
-      <input id='inputSearch' onChange={handleSearchChange} />
-      <ul id='listCountries'></ul>
-      <div id='result'></div>
-    </div>
-  );
-};
+import Search from './components/Search';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-
   useEffect(() => {
     console.log('effect');
     axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
