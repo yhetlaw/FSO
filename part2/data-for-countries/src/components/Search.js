80aff3ef8f3e54182812cee4a2897f41103263a9
yhetlaw <jandresantos14@gmail.com>
2021-09-09 22:31:32 +0100

    2.13 unfinished

--- part2/data-for-countries/src/components/Search.js
+++ part2/data-for-countries/src/components/Search.js
@@ -8,23 +8,68 @@ const TooManyCatches = (props) => {
   return <p>Too many catches, specify another filter!</p>;
 };
 
-const CountriesList = ({ status, matchedCountries }) => {
+const ListItem = ({ handleShowInfo, text, id }) => {
+  return (
+    <li>
+      {text}
+      <button data-id={id} onClick={handleShowInfo}>
+        Show info
+      </button>
+    </li>
+  );
+};
+
+const CountriesList = ({ status, matchedCountries, handleShowInfo }) => {
+  if (!status) {
+    return null;
+  }
+  return (
+    <div>
+      <ul>
+        {matchedCountries.map((country) => (
+          <ListItem
+            key={country.name}
+            text={country.name}
+            handleShowInfo={handleShowInfo}
+            id={country.name}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ShowInfo = ({ status, matchedCountries, id }) => {
   if (!status) {
     return null;
   }
 
-  const handleShowInfo = (event) => {
-    console.log('the button was clicked');
-  };
-
+  const mapedCountries = matchedCountries.map((country) => country.name);
+  console.log(id);
+  const test = mapedCountries.indexOf(id);
+  console.log(test);
   return (
-    <ul>
-      {matchedCountries.map((country) => (
-        <li key={country.name}>
-          {country.name} <button onClick={handleShowInfo}>Show info</button>{' '}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1>{matchedCountries[test].name}</h1>
+      <p>
+        The capital of {matchedCountries[test].name} is {matchedCountries[test].capital}
+      </p>
+      <p>
+        The population of {matchedCountries[test].name} is {matchedCountries[test].population}{' '}
+        people
+      </p>
+      <h2>Languages</h2>
+      <ul>
+        {matchedCountries[test].languages.map((language) => (
+          <li key={language.name}>{language.name}</li>
+        ))}
+      </ul>
+      <img
+        src={matchedCountries[test].flag}
+        alt={matchedCountries[test].name}
+        width='170'
+        height='150'></img>
+    </div>
   );
 };
 
@@ -61,11 +106,15 @@ const Search = ({ countries }) => {
 
   const [warning, setWarning] = useState(false);
   const [countryList, setCountryList] = useState(false);
-  const [fullCountry, setFullCountry] = useState(false);
+  const [fullCountryStatus, setFullCountryStatus] = useState(false);
   const [matchedCountries, setMatchedCountries] = useState([]);
+  const [newSearch, setNewSearch] = useState('');
+  const [clickedCountry, setClickedCountry] = useState(false);
+  const [id, setId] = useState('');
 
   const handleSearchChange = (event) => {
     const newSearch = event.target.value.toLowerCase();
+    setNewSearch(newSearch);
     const matchedCountries = countries.filter((country) =>
       country.name.toLowerCase().match(newSearch)
     );
@@ -73,11 +122,11 @@ const Search = ({ countries }) => {
 
     if (!inputSearch) {
       setWarning(true);
-      setFullCountry(false);
+      setFullCountryStatus(false);
       setCountryList(false);
     } else if (matchedCountries.length === 1) {
       setWarning(false);
-      setFullCountry(true);
+      setFullCountryStatus(true);
       setCountryList(false);
     } else if (
       matchedCountries.length > 0 &&
@@ -85,22 +134,46 @@ const Search = ({ countries }) => {
       matchedCountries.length !== 1
     ) {
       setWarning(false);
-      setFullCountry(false);
+      setFullCountryStatus(false);
       setCountryList(true);
+      setClickedCountry(false);
     } else {
       setWarning(true);
-      setFullCountry(false);
+      setFullCountryStatus(false);
       setCountryList(false);
     }
   };
+
+  const handleShowInfo = (event) => {
+    console.log('the button was clicked');
+    const id = event.target.getAttribute('data-id');
+    setId(id);
+
+    if (!clickedCountry) {
+      setClickedCountry(true);
+    } else {
+      setClickedCountry(false);
+    }
+  };
+
+  console.log(matchedCountries);
 
   return (
     <div>
       Search for countries:
       <input id='inputSearch' onChange={handleSearchChange} />
       <TooManyCatches status={warning} />
-      <CountriesList status={countryList} matchedCountries={matchedCountries} />
-      <FullCountry status={fullCountry} matchedCountries={matchedCountries} />
+      <CountriesList
+        status={countryList}
+        matchedCountries={matchedCountries}
+        handleShowInfo={handleShowInfo}
+      />
+      <FullCountry
+        status={fullCountryStatus}
+        matchedCountries={matchedCountries}
+        newSearch={newSearch}
+      />
+      <ShowInfo status={clickedCountry} matchedCountries={matchedCountries} id={id} />
     </div>
   );
 };
