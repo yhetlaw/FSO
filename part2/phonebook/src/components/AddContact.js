9572a80b0bf7694ca8953bac2ad898f9463fc92b
yhetlaw <jandresantos14@gmail.com>
2022-04-15 17:05:40 +0100

    Fixed logic for add contacts

--- part2/phonebook/src/components/AddContact.js
+++ part2/phonebook/src/components/AddContact.js
@@ -18,17 +18,23 @@ const AddContact = ({
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber,
+      number: Number(newNumber),
     }
 
-    //Map all names to uppercase and allNumbers
     const allNames = persons.map((person) => person.name.toUpperCase())
     const allNumbers = persons.map((person) => person.number)
-    //If allNames include newName or allNumber include newNumber
-    allNames.includes(newName.toUpperCase())
-      ? window.confirm(`The name ${newName} is already added to the phonebook, replace old number with the new one?`)
-        ? contactService
-            .update(persons.filter((person) => person.name === newName)[0].id, personObject)
+
+    if (allNames.includes(newName.toUpperCase())) {
+      if (
+        window.confirm(`The name ${newName} is already added to the phonebook, replace old number with the new one?`)
+      ) {
+        if (allNumbers.indexOf(+newNumber) !== -1) {
+          alert(`Number ${newNumber} is taken`)
+          document.getElementById('newNumber-input').value = ''
+        } else {
+          let personId = persons.filter((person) => person.name === newName)[0].id
+          contactService
+            .update(personId, personObject)
             .then((returnedPerson) => {
               setPersons(Object.assign(persons, personObject))
               setAlertMessage(`Contact ${newName} has been updated!`)
@@ -41,28 +47,32 @@ const AddContact = ({
               setLength(length - 1)
             })
             .catch((error) => {
-              setErrorMessage(`Information of ${newName} has already been removed previously!`)
+              setErrorMessage(`Contact ${newName} has not been updated.`)
               setTimeout(() => {
                 setAlertMessage(null)
               }, 3000)
             })
-        : console.log('fail')
-      : allNumbers.includes(newNumber)
-      ? alert(`Number ${newNumber} is taken`)
-      : contactService
-          .create(personObject)
-          .then((returnedPerson) => {
-            setPersons(persons.concat(returnedPerson))
-            setAlertMessage(`Contact ${newName} has been added!`)
-            setTimeout(() => {
-              setAlertMessage(null)
-            }, 3000)
-            setNewName('')
-            setNewNumber('')
-          })
-          .catch((error) => {
-            setErrorMessage(error.response.data.error)
-          })
+        }
+      }
+    } else if (allNumbers.indexOf(+newNumber) !== -1) {
+      alert(`Number ${newNumber} is taken`)
+      document.getElementById('newNumber-input').value = ''
+    } else {
+      contactService
+        .create(personObject)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson))
+          setAlertMessage(`Contact ${newName} has been added!`)
+          setTimeout(() => {
+            setAlertMessage(null)
+          }, 3000)
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch((error) => {
+          setErrorMessage(error.response.data.error)
+        })
+    }
   }
 
   return (
@@ -72,7 +82,7 @@ const AddContact = ({
           name: <input value={newName} onChange={onNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={onNumberChange} />
+          number: <input value={newNumber} onChange={onNumberChange} id='newNumber-input' />
         </div>
         <div>
           <button type='submit'>add</button>
