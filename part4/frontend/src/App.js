fda3eecb06aacfc472495a14721a9d05cee47828
yhetlaw <jandresantos14@gmail.com>
2022-04-25 19:57:56 +0100

    part4 first commit

---
+++ part4/frontend/src/App.js
@@ -0,0 +1,26 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Blog List</h1>
+      <form>
+        <div>
+          Author: <input />
+        </div>
+        <div>
+          Title: <input />
+        </div>
+        <div>
+          Url: <input />
+        </div>
+        <div>
+          Upvotes: <input />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
