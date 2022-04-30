b8032f3fd88d76ccfaebd4917d28570b7b0ad1b5
yhetlaw <jandresantos14@gmail.com>
2022-04-30 20:32:15 +0100

    Added ESlint, 4.3: helper functions and unit tests, step1 finished

---
+++ part4/backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: 'off',
+    'linebreak-style': 0,
+    semi: ['error', 'never'],
+    'no-console': 0,
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+  },
+}
