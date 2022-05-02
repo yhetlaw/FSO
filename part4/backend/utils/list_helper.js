29fcf0a780bf7f1b2f3d28ddeabd41ac5fd38320
yhetlaw <jandresantos14@gmail.com>
2022-05-02 18:05:25 +0100

    Added favorite blog test

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,14 +2,29 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogPosts) => {
+const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item
   }
-  return blogPosts.reduce(reducer, 0)
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes)
+  const favorite = likesArray.reduce((a, b) => {
+    return Math.max(a, b)
+  }, -Infinity)
+  const foundFavorite = blogs.find((blogs) => blogs.likes == favorite)
+
+  return {
+    author: foundFavorite.author,
+    likes: foundFavorite.likes,
+    title: foundFavorite.title,
+  }
 }
 
 module.exports = {
+  favoriteBlog,
   dummy,
   totalLikes,
 }
