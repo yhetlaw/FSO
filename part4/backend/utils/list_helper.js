219002f8fc0bf04b8ff9ae3da8132821372a9314
yhetlaw <jandresantos14@gmail.com>
2022-05-02 04:25:40 +0100

    Added totalLikes test

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return blogPosts.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
